6b15464c08b0f5716008cbd9b989838eafb74d5f
BaiXF <baixuefeng1127@gmail.com>
2014-08-25 02:30:39 +0800

    backend implementation
    Storage first

---
+++ src/storage/Data.java
@@ -0,0 +1,31 @@
+package storage;
+
+import java.util.ArrayList;
+
+public class Data implements IData {
+
+	@Override
+	public void addWordsToIgnore(String s) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addTitles(String s) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getIndex(String s) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public ArrayList<String> getSortedList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- src/storage/Title.java
+++ src/storage/Title.java
@@ -1,5 +1,28 @@
 package storage;
 
 public class Title {
-
+	
+	public Title(String str, int i){
+		line = str;
+		index = i;
+	}
+	
+	private void setLine(String str){
+		line = str;
+	}
+	
+	private void setIndex(int i){
+		index = i;
+	} 
+	
+	private String getLine(){
+		return line;
+	}
+	
+	private int getIndex(){
+		return index;
+	}
+	
+	private int index;
+	private String line;
 }
