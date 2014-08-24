a77807f5c622aa85cb9ef09ee04351541eb63c81
BaiXF <baixuefeng1127@gmail.com>
2014-08-25 03:24:09 +0800

    implement data draft
    instead of saving AL of Title in data, I used HashMap(String line,
    Integer Index).
    It makes getIndex in O(1) and does not affect other functionalities

--- src/storage/Data.java
+++ src/storage/Data.java
@@ -1,31 +1,38 @@
 package storage;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Data implements IData {
 
 	@Override
 	public void addWordsToIgnore(String s) {
 		// TODO Auto-generated method stub
-		
+		stopWords.add(s);
 	}
 
 	@Override
 	public void addTitles(String s) {
 		// TODO Auto-generated method stub
-		
+		titleMap.put(s, nextIndex);
+		nextIndex++;
 	}
 
 	@Override
 	public int getIndex(String s) {
 		// TODO Auto-generated method stub
-		return 0;
+		return titleMap.get(s);
 	}
 
-	@Override
+	@Override									// What is this for???
 	public ArrayList<String> getSortedList() {
 		// TODO Auto-generated method stub
+//		return Sorter.sort();
 		return null;
 	}
+	
+	private ArrayList<String> stopWords = new ArrayList<String>();
+	private HashMap<String, Integer> titleMap = new HashMap<String, Integer>(); 
+	private int nextIndex = 0;
 
 }
