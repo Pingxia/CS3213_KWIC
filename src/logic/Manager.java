36795525457818d2512d58ce5c5eca2aa0460a46
BaiXF <baixuefeng1127@gmail.com>
2014-08-25 17:49:17 +0800

    implement logic

---
+++ src/logic/Manager.java
@@ -0,0 +1,19 @@
+package logic;
+
+import java.util.ArrayList;
+
+public class Manager implements IManager {
+
+	@Override
+	public void processLine(String s) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ArrayList<String> execute() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
