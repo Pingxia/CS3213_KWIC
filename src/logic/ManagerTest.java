f25987ae26d9d49b59ffb652b5fd6bc5998ac1e8
BaiXF <baixuefeng1127@gmail.com>
2014-08-28 11:24:52 +0800

    create JUnit tests for Logic components

---
+++ src/logic/ManagerTest.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package logic;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author asus
+ *
+ */
+public class ManagerTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
